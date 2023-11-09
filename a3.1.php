a9f52347b5c8c5b8d28b4448f26a75756f43afcd
Daniel <danielsanchezgonzalez@alumno.ieselrincon.es>
2023-11-09 18:14:33 +0000

    cambios menores en el menu

--- a3.1.php
+++ a3.1.php
@@ -18,7 +18,7 @@ if (isset($_POST['crear'])) {
 } 
 /* De momento, estarán comentadas porque no se han creado los archivos correspondientes.
 elseif (isset($_POST['elimina'])) {
-    header("Location: elimina_producto.php");
+    header("Location: eliminar_producto.php");
 }
 */
 ?>
@@ -27,11 +27,11 @@ elseif (isset($_POST['elimina'])) {
 <form method="post">
     <button id="crear" style="color: white; background-color: rgb(133, 7, 7); width:300; height:50" name="crear">Crear un producto</button>
     <br><br><br>
-    <button id="list" style="color: white; background-color: rgb(133, 7, 7); width:300; height:50" name="list">Consultar Listado de Productos</button>
+    <button id="list" style="color: white; background-color: rgb(133, 7, 7); width:300; height:50" name="list">Consultar Listado de productos</button>
     <br><br><br>
-    <button id="mod" style="color: white; background-color: rgb(133, 7, 7); width:300; height:50" name="mod">Modificar Producto</button>
+    <button id="mod" style="color: white; background-color: rgb(133, 7, 7); width:300; height:50" name="mod">Modificar producto</button>
     <br><br><br>
-    <button id="elimina" style="color: white; background-color: rgb(133, 7, 7); width:300; height:50" name="elimina">Eliminar Producto</button>
+    <button id="elimina" style="color: white; background-color: rgb(133, 7, 7); width:300; height:50" name="elimina">Eliminar producto</button>
 </form>
 
 
